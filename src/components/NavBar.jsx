81cdc498a39a600450d7c42f176482f7583cd90d
aliceaa396 <ala31996@gmail.com>
2026-06-08 09:28:49 -0400

    updated info icon class name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,3 @@
-// export default function NavBar({ current, navigate, s, t }) {
-//   const items = ["Home", "Recipes", "Methods", "Profile"];
-//   return (
-//     <div style={{
-//       ...s.navBar,
-//       position: "sticky",
-//       bottom: 0,
-//       background: t.bg,
-//       borderTop: `1px solid ${t.border}`,
-//       marginTop: 28,
-//       zIndex: 100,
-//     }}>
-//       {items.map(item => (
-//         <div key={item} style={s.navItem(current === item)} onClick={() => navigate(item)}>
-//           <div style={s.navLine(current === item)} />
-//           {item}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
 export default function NavBar({ current, navigate, s, t }) {
   const items = ["Home", "Recipes", "Journal", "Methods", "Profile"];
   return (
